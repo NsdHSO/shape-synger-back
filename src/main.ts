a981a1449baacf7d8ea1fa21d608248e4b365bf7
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2024-04-29 17:12:20 +0300

    chore:🌻 add ionic capacitor

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 declare const module: any;
-const allowlist = ['http://localhost:4200','https://shape-synger'];
+const allowlist = [
+  'http://localhost:4200',
+  'capacitor://localhost',
+  'ionic://localhost',
+];
 const corsOptionsDelegate = function (req, callback) {
-  console.log(req.header('Origin'))
+  console.log(req.header('Origin'));
   let corsOptions;
   if (allowlist.indexOf(req.header('Origin')) !== -1) {
     corsOptions = { origin: false }; // reflect (enable) the requested origin in the CORS response
@@ -17,7 +21,7 @@ const corsOptionsDelegate = function (req, callback) {
   callback(null, corsOptions); // callback expects two parameters: error and options
 };
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false});
+  const app = await NestFactory.create(AppModule, { cors: false });
 
   const config = new DocumentBuilder()
     .setTitle('Farm')
