cc8f3a1447d7b57f4689f196e884544a733a95fb
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2024-04-29 15:56:54 +0300

    chore:🌻 add false for cors

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,17 @@ import { AppModule } from './app.module';
 declare const module: any;
 const allowlist = ['http://localhost:4200','https://shape-synger'];
 const corsOptionsDelegate = function (req, callback) {
+  console.log(req.header('Origin'))
   let corsOptions;
   if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+    corsOptions = { origin: false }; // reflect (enable) the requested origin in the CORS response
   } else {
     corsOptions = { origin: false }; // disable CORS for this request
   }
   callback(null, corsOptions); // callback expects two parameters: error and options
 };
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true});
+  const app = await NestFactory.create(AppModule, { cors: false});
 
   const config = new DocumentBuilder()
     .setTitle('Farm')
