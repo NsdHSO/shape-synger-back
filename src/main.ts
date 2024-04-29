5758335dc2e587f784bb8c864063aaa006751e49
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2024-04-29 17:36:38 +0300

    chore:🌻 add true variable for cors

--- src/main.ts
+++ src/main.ts
@@ -14,9 +14,9 @@ const corsOptionsDelegate = function (req, callback) {
   console.log(req.header('Origin'));
   let corsOptions;
   if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: false }; // reflect (enable) the requested origin in the CORS response
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
   } else {
-    corsOptions = { origin: false }; // disable CORS for this request
+    corsOptions = { origin: true }; // disable CORS for this request
   }
   callback(null, corsOptions); // callback expects two parameters: error and options
 };
