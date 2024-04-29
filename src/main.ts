24a3eb8bc197fba73158da5c22a6cc9e78a98057
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2024-04-29 07:51:16 +0300

    chore:🌻 add headers in vercel.json and cors in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,18 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 declare const module: any;
+const allowlist = ['http://localhost:4200','https://localhost'];
 const corsOptionsDelegate = function (req, callback) {
-  callback(null, { origin: false }); // callback expects two parameters: error and options
+  let corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
 };
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(corsOptionsDelegate);
 
   const config = new DocumentBuilder()
     .setTitle('Farm')
@@ -21,6 +27,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  app.enableCors(corsOptionsDelegate);
 
   await app.listen(3000);
 
